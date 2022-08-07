dd69d2775e183c62259fa2e16ccd537ccf478352
marcfrankel <mfrankel218@gmail.com>
2022-08-06 21:32:52 -0400

    Initial saving of breeze number

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useIsFocused } from '@react-navigation/native';
+import { primary } from '../theme';
 
 export default function HomeScreen() {
+    const [loaded, setLoaded] = useState(false);
+    const [cardNumber, setCardNumber] = useState<string | undefined>(undefined);
+    const isFocused = useIsFocused();
+
+    useEffect(() => {
+        const loadData = async () => {
+            const item = await AsyncStorage.getItem('breezeCardNumber');
+            if (item !== '' || item !== undefined) {
+                setCardNumber(item);
+            }
+            setLoaded(true);
+        };
+        loadData();
+    }, [isFocused]);
+
     return (
         <View
             style={{
@@ -10,7 +28,30 @@ export default function HomeScreen() {
                 alignItems: 'center',
             }}
         >
-            <Text>Home Screen</Text>
+            {!loaded ? (
+                <>
+                    <ActivityIndicator
+                        size="large"
+                        color={primary.text.secondary}
+                    />
+
+                    <Text
+                        style={{
+                            fontSize: 25,
+                            marginTop: 10,
+                            fontFamily: 'Helvetica Neue Bold',
+                            color: primary.text.primary,
+                        }}
+                    >
+                        Loading...
+                    </Text>
+                </>
+            ) : (
+                <>
+                    <Text>Home Screen2</Text>
+                    {cardNumber !== undefined && <Text>{cardNumber}</Text>}
+                </>
+            )}
         </View>
     );
 }
--- screens/Settings.tsx
+++ screens/Settings.tsx
@@ -1,16 +1,103 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import {
+    Platform,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { primary } from '../theme';
 
 export default function SettingsScreen() {
+    const [cardNumber, setCardNumber] = useState('');
+    const navigator = useNavigation();
+
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
+        <KeyboardAwareScrollView
+            extraScrollHeight={Platform.OS === 'ios' ? 50 : 0}
+            contentContainerStyle={{ flexGrow: 1 }}
+            keyboardShouldPersistTaps="never"
+            showsVerticalScrollIndicator={false}
         >
-            <Text>Settings Screen</Text>
-        </View>
+            <View
+                style={{
+                    flex: 1,
+                    paddingHorizontal: 12,
+                    marginTop: 20,
+                }}
+            >
+                <View
+                    style={{
+                        width: '100%',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontFamily: 'Helvetica Neue Bold',
+                            fontSize: 16,
+                        }}
+                    >
+                        BreezeCard Number:
+                    </Text>
+                    <TextInput
+                        placeholder="card number"
+                        keyboardType="numeric"
+                        style={{
+                            flexGrow: 1,
+                            marginLeft: 20,
+                            fontSize: 16,
+                            backgroundColor: primary.text.secondary,
+                            borderRadius: 4,
+                            color: primary.text.primary,
+                            padding: 4,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            height: 25,
+                            fontFamily: 'Helvetica Neue Medium',
+                        }}
+                        onChangeText={(value) => {
+                            setCardNumber(value);
+                        }}
+                    />
+                </View>
+                <TouchableOpacity
+                    style={{
+                        width: 'auto',
+                        flexDirection: 'row',
+                        padding: 15,
+                        margin: 20,
+                        marginTop: 'auto',
+                        marginBottom: 50,
+                        borderRadius: 10,
+                        backgroundColor: primary.orange,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                    onPress={async () => {
+                        await AsyncStorage.setItem(
+                            'breezeCardNumber',
+                            cardNumber
+                        );
+                        navigator.goBack();
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontFamily: 'Helvetica Neue Bold',
+                            fontSize: 20,
+                            color: primary.white,
+                        }}
+                    >
+                        Save!
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAwareScrollView>
     );
 }
